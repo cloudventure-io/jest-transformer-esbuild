e94f55ad2559a99b2b1e71231cb10616f1a4c33a
Vahe Sahakyan <vahe@spir.me>
2023-02-19 19:16:46 +0100

    tests: include typescript code in tests

--- index.test.ts
+++ index.test.ts
@@ -2,9 +2,13 @@ import { jest } from "@jest/globals";
 
 const mock = jest.fn();
 
+type TestType = string;
+
 describe("basic", () => {
+  const test: TestType = "test";
   it("check if mocks are working", () => {
     mock.mockImplementation(() => "test");
     expect(mock()).toBe("test");
+    expect(test).toBe("test");
   });
 });
